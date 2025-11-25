a226b79cf48c5b49a5b99fec900df46a2781a936
GuilhermeGracioto <149427983+GuilhermeGracioto@users.noreply.github.com>
2025-11-25 17:34:16 -0400

    Arrumado infernp

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-// Use "type: commonjs" in package.json to use CommonJS modules
 const express = require('express');
 const app = express();
 const port = 3000;
  
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set('view engine','ejs')
+app.set('views','./views')
 
-// Define your routes
 require("../db/dbconnect")(app)
 require("../rotas/home")(app)
 
@@ -15,9 +13,9 @@ require("../rotas/getidnoticias")(app)
 require("../rotas/gettiponoticias")(app)
 
 require("../rotas/postnoticias")(app)
-require("../rotas/delnoticias")(app)
 require("../rotas/putnoticias")(app)
- 
+require("../rotas/delnoticias")(app)
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
